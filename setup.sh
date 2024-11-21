672c0261a13722f53005dadd463d8722e5cca1ce
my <my@my.my>
2024-11-22 01:44:48 +0800

    .icons

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 ln -s ~/static/fcitx5 ~/.local/share/
 ln -s ~/static/icons ~/.local/share/
-ln -s ~/static/icons ~/.icons
+ln -s ~/static/.icons ~/
 ln -s ~/static/fonts ~/.local/share/
 ln -s ~/static/Kvantum ~/.config/
