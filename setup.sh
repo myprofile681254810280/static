0f1fb21b15639803c5756405d15b7ac0366307dd
my <my@my.my>
2024-11-22 16:00:45 +0800

    kwinrulesrc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+ln -s ~/static/.icons ~/
+
 ln -s ~/static/fcitx5 ~/.local/share/
 ln -s ~/static/icons ~/.local/share/
-ln -s ~/static/.icons ~/
 ln -s ~/static/fonts ~/.local/share/
+ln -s ~/static/color-schemes ~/.local/share/
+
 ln -s ~/static/Kvantum ~/.config/
+ln -s ~/static/kwinrulesrc ~/.config/
