68639bb823186b8fa6c561ccff1b64b29f36a998
my <my@my.my>
2024-11-22 17:16:29 +0800

    dolphin

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -s $ROOT/local-share/fcitx5          ~/.local/share/
 ln -s $ROOT/local-share/icons           ~/.local/share/
 ln -s $ROOT/local-share/fonts           ~/.local/share/
 ln -s $ROOT/local-share/color-schemes   ~/.local/share/
+ln -s $ROOT/local-share/kxmlgui5        ~/.local/share/
 
 ln -s $ROOT/config/Kvantum              ~/.config/
 ln -s $ROOT/config/kwinrulesrc          ~/.config/
