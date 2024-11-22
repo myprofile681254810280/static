68578f6afbd144478e81df9cdb28a5ce7bb717ed
my <my@my.my>
2024-11-22 16:05:53 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-ln -s ~/static/.icons ~/
+ln -s ~/static/home/.icons ~/
 
-ln -s ~/static/fcitx5 ~/.local/share/
-ln -s ~/static/icons ~/.local/share/
-ln -s ~/static/fonts ~/.local/share/
-ln -s ~/static/color-schemes ~/.local/share/
+ln -s ~/static/local-share/fcitx5 ~/.local/share/
+ln -s ~/static/local-share/icons ~/.local/share/
+ln -s ~/static/local-share/fonts ~/.local/share/
+ln -s ~/static/local-share/color-schemes ~/.local/share/
 
-ln -s ~/static/Kvantum ~/.config/
-ln -s ~/static/kwinrulesrc ~/.config/
+ln -s ~/static/config/Kvantum ~/.config/
+ln -s ~/static/config/kwinrulesrc ~/.config/
