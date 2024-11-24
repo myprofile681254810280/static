6c10d2e5027ef450078aa61e3921c68a67d255a3
my <my@my.my>
2024-11-24 12:11:25 +0800

    wallpapers/

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ mv ~/.icons /dev/shm/
 mv ~/.local/share/icons /dev/shm/
 mv ~/.local/share/fonts /dev/shm/
 mv ~/.local/share/fcitx5 /dev/shm/
+mv ~/.local/share/wallpapers /dev/shm/
 
 ln -s "$ROOT/.icons"          ~/
 ln -s "$ROOT/icons"          ~/.local/share/
 ln -s "$ROOT/fcitx5"         ~/.local/share/
 ln -s "$ROOT/fonts"          ~/.local/share/
+ln -s "$ROOT/wallpapers"          ~/.local/share/
